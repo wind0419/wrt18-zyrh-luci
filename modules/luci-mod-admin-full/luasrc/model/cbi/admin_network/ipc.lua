6458ac622406c1f1d76477452c33b2c4533cc388
wind <wind@geely.com>
2019-11-29 16:23:21 +0800

    add video type and html showing video result

--- modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/ipc.lua
+++ modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/ipc.lua
@@ -1,6 +1,8 @@
 
 local fs = require "nixio.fs"
-local m,s,olt,opt
+local m,s,olt,opt,port,preview
+local val 
+local info_val
 
 m = Map("ipc_cfg", translate("IPC"), translate("Setting IPC args"))
 s = m:section(TypedSection, "ipc_cfg", translate("IPC args"))
@@ -17,25 +19,41 @@ opt.rmempty = false
 opt:depends("enable", "1")
 
 opt = s:taboption(this_tab, Value, "ipaddr", translate("IPC Ipaddr"))
+opt.rmempty = false
 opt.datatype = "ip4addr"
 opt.placeholder = "192.168.1.10"
 opt:depends("enable", "1")
 
-opt = s:taboption(this_tab, Value, "port", translate("IPC Port"))
-opt.placeholder = "34567"
-opt.datatype    = "port"
-opt:depends("enable", "1")
+port = s:taboption(this_tab, Value, "port", translate("IPC Port"))
+opt.rmempty = false
+port.placeholder = "34567"
+port.datatype    = "port"
+port:depends("enable", "1")
 
---opt = s:taboption(this_tab, DummyValue, "gps_info", translate("GPS Info"))
---opt.default="init..."
---opt:depends("enable", "1")
---function opt.cfgvalue(...)
---	return fs.readfile("/tmp/gps_info") or "init..."
---end
+opt = s:taboption(this_tab, ListValue, "search_type", translate("Video Type"))
+opt.default = 1
+opt:value(0, translate("ALL Video"))
+opt:value(1, translate("ALARM Video"))
+opt:value(2, translate("DETECT Video"))
+opt:value(3, translate("REGULAR Video"))
+opt:value(4, translate("MANUAL Video"))
+opt:value(5, translate("KEY Video"))
+
+--- HTTP preview  ---
+html_h = [[
+<div>
+<p>IPC History Record:</p>
+<textarea style="font-size: 12px; width: 640px; height: 320px;" readonly="readonly" wrap="off" id="record">
+]]
+html_t= [[
+</textarea></div>
+]]
+
+info_val = fs.readfile("/tmp/record_info") or ""
+val = html_h..info_val..html_t
+preview = s:taboption(this_tab, DummyValue, "_dummy", val)
+preview:depends("enable", "1")
 
 local apply = luci.http.formvalue("cbi.apply")
---if(apply) then
---	io.popen("/etc/init.d/net4g restart &")
---end
 
 return m
